aaba8488db6b75fe57dcd7c434675c9f60db61f4
YanethZG <jeanethzelaya56@gmail.com>
2022-05-26 23:20:28 -0600

    Agregando la funcion de filtros usuarios

--- models/usuario.php
+++ models/usuario.php
@@ -27,5 +27,31 @@ function deleteUser($id) //5
 
 function filterUsers($filter) //6
 {
+    $conn = connect();
+    $row = array();
+    $string = "SELECT id_usuario, nombre, apellido, email, id_rol FROM 
+    usuarios WHERE (nombre LIKE '%$filter%' 
+    OR apellido LIKE '%$filter%' OR email LIKE '%$filter%') ORDER BY nombre ASC";
+    
+    $query = mysqli_query($conn, $string);
+    $nRow = mysqli_num_rows($query);
 
+    if ($nRow != 0) {
+        while ($Usuarios = mysqli_fetch_array($query)) {
+            $jsonRow = array();
+            $id_usuario = $Usuarios["id_usuario"];
+            $nombre = $Usuarios["nombre"];
+            $apellido = $Usuarios["apellido"];
+            $email = $Usuarios["email"];
+            $id_rol = $Usuarios["id_rol"];
+            $jsonRow["id_usuario"] = $id_usuario;
+            $jsonRow["nombre"] = $nombre;
+            $jsonRow["apellido"] = $apellido;
+            $jsonRow["email"] = $email;
+            $jsonRow["id_rol"] = $id_rol;
+            $row[] = $jsonRow;
+        }
+}
+disconnect($conn);
+    return array_values($row);
 }
